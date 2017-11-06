074dd2f93b45041c8cd8d589fcc022af02eb9419
Unknown <frn@o2.pl>
2017-11-06 21:51:57 +0100

    netctl profiles menu improvement

--- pipemenus/netctl-list.sh
+++ pipemenus/netctl-list.sh
@@ -1,36 +1,58 @@
 #!/bin/bash
 
-to_execute() {
+function generateExecute() {
 	printf '  <item label="%s">\n' "$1"
-	printf '    <action name="Execute"><command>sudo netctl %s %s</command></action>\n' "$1" "$2"
+	printf '    <action name="Execute"><command>%s</command></action>\n' "$2"
 	printf '  </item>\n'
 }
 
-to_menu() {
+function generateMenu() {
 	declare name="${1}"
 	declare -a exec=("${!2}")
 	printf '<menu id="%s" label="%s">\n' "$name" "$name"
 	for c in "${exec[@]}"; do
-	    to_execute ${c} ${name}
+	    command="${NETCTL} ${c} ${name}"
+	    generateExecute ${c} ${command}
 	done
-	printf '  </menu>\n'
+	printf '</menu>\n'
 }
 
-netctl_prof() {
-profiles=($(netctl list))
+function generateSeparator() {
+    [[ ${1} ]] && printf '  <separator label="%s"/>\n' "$1" || echo "<separator />"
 
-regex="^\*"
-for p in "${profiles[@]}"; do
-	cmd=("start")
-	[[ $p =~ ^\* ]] && cmd=("stop" "restart")
-	to_menu "${p// /}" cmd[@]
-done
+}
+
+function connectedProfile() {
+    p=$(netctl list | grep \*)
+    [[ ${p} ]] && p="${CONNECTED} ${p//\*/}" || p="${CONNECTED} []"
+    generateSeparator ${p}
+}
+
+function profiles() {
+    profiles=($(netctl list))
+    regex="^\*"
+    CMD="start"
+
+    [[ `echo ${profiles[@]} | grep \*` ]] && CMD="switch-to"
+
+    for p in "${profiles[@]}"; do
+	    cmd=(${CMD})
+	    [[ $p =~ ^\* ]] && cmd=("stop" "restart")
+	    p=${p//\*/}
+	    p=${p// /}
+	    generateMenu "${p}" cmd[@]
+    done
 }
 
 IFS=$'\n'
+NETCTL="sudo netctl"
+CONNECTED="Connected profile:"
+
 
 echo '<?xml version="1.0" encoding="UTF-8"?>'
 echo '<openbox_pipe_menu>'
-to_execute 'stop-all'
-netctl_prof
+connectedProfile
+generateExecute "Stop All" "${NETCTL} stop-all"
+generateSeparator "Profiles"
+profiles
 echo '</openbox_pipe_menu>'
