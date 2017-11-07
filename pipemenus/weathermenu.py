759344444db8209cb226690deadbcd6080f20a47
Unknown <frn@o2.pl>
2017-11-07 23:25:05 +0100

    refactor

--- pipemenus/weathermenu.py
+++ pipemenus/weathermenu.py
@@ -12,83 +12,85 @@ import httplib2
 
 
 class WeatherBit(object):
-    CACHE_HOURS = 1
-    weatherUrl = 'http://api.weatherbit.io/v2.0'
-    currentWeather = weatherUrl + '/current'
-    cache_file = join(os.getenv("HOME"), '.weatherbit.cache')
-    key_file = join(os.getenv("HOME"), '.weatherbit')
-    city = 'Wroclaw'
-    _lang = 'en'
+    _CACHE_HOURS = 1
     _key = ''
-    http = httplib2.Http()
-    content = ''
-    menu_string = ''
-    cache = {}
-    facts = ['temp', 'description', 'wind_cdir', 'sunset', 'sunrise', 'clouds']
-    translations = {'temp': 'Temperature', 'description': 'Description', 'wind_cdir': 'Wind', 'sunset': 'Sunset',
-                    'sunrise': 'Sunrise', 'clouds': 'Clouds'}
-    factsDir = OrderedDict()
+    _weatherUrl = 'http://api.weatherbit.io/v2.0'
+    _currentWeather = _weatherUrl + '/current'
+    _cacheFile = join(os.getenv("HOME"), '.weatherbit.cache')
+    _keyFile = join(os.getenv("HOME"), '.weatherbit')
+    _content = ''
+    _cache = {}
+    _facts = ['temp', 'description', 'wind_cdir', 'wind_spd', 'sunset', 'sunrise', 'clouds']
+    _translations = {'temp': 'Temperature', 'description': 'Description', 'wind_cdir': 'Wind', 'wind_spd': 'Wind Speed', 'sunset': 'Sunset',
+                     'sunrise': 'Sunrise', 'clouds': 'Clouds'}
+    _factsDir = OrderedDict()
+    _menu_string = ''
+    city = ''
+    lang = 'en'
 
     def get_dev_key(self):
-        self._key = open(self.key_file, 'r').read().strip('\n')
-
-    def set_city(self, city):
-        self.city = city
-        return self
+        self._key = open(self._keyFile, 'r').read().strip('\n')
 
     def get_weather(self):
-        params_dict = {'lang': self._lang, 'city': self.city, 'key': self._key}
+        self.get_dev_key()
+        params_dict = {'lang': self.lang, 'city': self.city, 'key': self._key}
         params = urllib.urlencode(params_dict)
-        resp, content = self.http.request(self.currentWeather + '?' + params)
-        self.content = json.loads(content)['data'][0]
+        resp, content = httplib2.Http().request(self._currentWeather + '?' + params)
+        self._content = json.loads(content)['data'][0]
 
     def set_facts(self):
         self.get_weather()
-        for fact in self.facts:
+        for fact in self._facts:
             if fact == 'description':
-                self.factsDir[self.translations[fact]] = self.content['weather'][fact]
+                self._factsDir[self._translations[fact]] = self._content['weather'][fact]
             else:
-                self.factsDir[self.translations[fact]] = self.content[fact]
-
-    def __init__(self, city):
-        self.get_dev_key()
-        self.set_city(city)
-        self.generate_menu()
+                self._factsDir[self._translations[fact]] = self._content[fact]
 
     def generate_menu(self):
         self.read_cache()
-        if not self.cache or self.city not in self.cache or (
-                        self.cache[self.city]['date'] + timedelta(hours=self.CACHE_HOURS) < datetime.utcnow()):
+        if not self._cache or self.city not in self._cache or self.outdated_cache():
             self.set_facts()
-            self.menu_string = ' <openbox_pipe_menu>\n'
-            self.menu_string += '\t<separator label = "%s" />\n' % self.city
-            for fact, value in self.factsDir.iteritems():
-                self.menu_string += '\t\t<item label="%s: %s"/>\n' % (fact, value)
-            self.menu_string += ' </openbox_pipe_menu>\n'
+            self._menu_string = ' <openbox_pipe_menu>\n'
+            self._menu_string += '\t<separator label = "%s" />\n' % self.city
+            for fact, value in self._factsDir.iteritems():
+                self._menu_string += '\t\t<item label="%s: %s"/>\n' % (fact, value)
+            self._menu_string += ' </openbox_pipe_menu>\n'
             self.write_cache()
         else:
-            self.menu_string = self.cache[self.city]['ob_pipe_menu']
+            self._menu_string = self._cache[self.city]['ob_pipe_menu']
+
+    def outdated_cache(self):
+        return self._cache[self.city]['date'] + timedelta(hours=self._CACHE_HOURS) < datetime.utcnow()
 
     def write_cache(self):
         try:
-            self.cache[self.city] = {'date': datetime.utcnow(), 'ob_pipe_menu': self.menu_string}
-            f = open(self.cache_file, 'wb')
-            pickle.dump(self.cache, f, -1)
+            self._cache[self.city] = {'date': datetime.utcnow(), 'ob_pipe_menu': self._menu_string}
+            f = open(self._cacheFile, 'wb')
+            pickle.dump(self._cache, f, -1)
             f.close()
         except IOError:
             raise
 
     def read_cache(self):
         try:
-            f = open(self.cache_file, 'rb')
-            self.cache = pickle.load(f)
+            f = open(self._cacheFile, 'rb')
+            self._cache = pickle.load(f)
             f.close()
         except IOError:
-            self.cache = {}
+            self._cache = {}
+
+    def __init__(self, city):
+        self.city = city
+        self.generate_menu()
+
+    @property
+    def menu_string(self):
+        return self._menu_string
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 1:
         print('No chosen city')
         exit(1)
-    print (WeatherBit(sys.argv[1]).menu_string)
+    weather_bit = WeatherBit(sys.argv[1])
+    print (weather_bit.menu_string)
